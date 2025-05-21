5d94dc4923acf6946e2b2cb2bc1e1fe40c838cc3
Arthur Freitas <freitarthur176@gmail.com>
2025-05-21 16:26:35 -0300

    update
    update

--- dia-03/projeto1/js/script.js
+++ dia-03/projeto1/js/script.js
@@ -1,1 +1,9 @@
+// Recuperar o elemento pai
 
+const footer = document.querySelector('footer')
+
+// Adicionando última linha
+
+const spanText = document.createElement('span')
+spanText.innerHTML = '2025'
+footer.appendChild(spanText)
