29fc9143ef9e5bdd138aaed6278745352c3ff76e
Arthur Freitas <freitarthur176@gmail.com>
2025-05-22 14:33:13 -0300

    Update
    Update

--- dia-03/projeto1/js/script.js
+++ dia-03/projeto1/js/script.js
@@ -3,3 +3,42 @@ const footer = document.querySelector('footer')
 const spanText = document.createElement('span')
 spanText.innerHTML = 'Criado no ano de 2025'
 footer.appendChild(spanText)
+
+const body = document.querySelector('body')
+const main = document.querySelector('main')
+console.log(main);
+
+const buttonTheme = document.getElementById('button-theme')
+const buttonForms = document.getElementById('button')
+
+window.onload = () => {
+    const themeSave = localStorage.getItem('theme')
+    if (themeSave == 'light') {
+        body.style.backgroundColor = '#fff'
+        main.style.color = 'black'
+        buttonTheme.innerHTML = '🌙'
+    } else if (themeSave == 'dark') {
+        body.style.backgroundColor = "#000"
+        main.style.color = '#fff'
+        buttonTheme.innerHTML = '☀️'
+    } else {
+        localStorage.setItem('theme', 'light')
+    }
+}
+
+const alterTheme = () => {
+    const themeSave = localStorage.getItem('theme')
+    if (themeSave == 'light') {
+        body.style.backgroundColor = '#000'
+        main.style.color = '#fff'
+        buttonTheme.innerHTML = '☀️'
+        localStorage.setItem('theme', 'dark')
+    } else {
+        body.style.backgroundColor = '#fff'
+        main.style.color = '#000'
+        buttonTheme.innerHTML = '🌙'
+        localStorage.setItem('theme', 'light')
+    }
+}
+
+buttonTheme.addEventListener('click', alterTheme)
